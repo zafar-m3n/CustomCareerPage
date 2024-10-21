825a345e229587f0cf275bfe45b857b541a06381
zafar-m3n <thezafar.m3n@gmail.com>
2024-10-21 14:55:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Career Page Customization
+      </h1>
+    </div>
+  );
+};
+
+export default App;
